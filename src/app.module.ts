2ae9a699b8667004ee4e6cfe0a1cad0045a47738
wsp19960527 <45484227+wsp19960527@users.noreply.github.com>
2022-11-28 17:04:23 +0800

    初始化项目

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RegisterModule } from './register/register.module';
 
 @Module({
-  imports: [],
+  imports: [RegisterModule],
   controllers: [AppController],
   providers: [AppService],
 })
